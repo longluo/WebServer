03a43a5f24de9c7f00cfbda539ea2564edeebcb7
longluo <luolong0226@163.com>
2024-01-30 11:40:39 +0800

    the webserver framework: automake.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # WebServer
+
+https://bruinsslot.jp/post/simple-http-webserver-in-c/
+
+
+https://blog.orhun.dev/zig-bits-04/
+
+
+https://github.com/JeffreytheCoder/Simple-HTTP-Server
+
+
+
+
+
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include <dirent.h>
+#include <unistd.h>
+
+#include <iostream>
+#include <string>
+
+
+int main(int argc, const char *argv[]) {
+	std::cout << "Hello WebServer!" << std::endl;
+	return 0;
+}
