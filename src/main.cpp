7d4b66d1b4c138452149b3cae8e3a149d6175694
longluo <luolong0226@163.com>
2024-01-30 11:55:08 +0800

    the socket bind.

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,36 @@
 
 #include <errno.h>
 
+#include <arpa/inet.h>
 #include <sys/socket.h>
 
 
+#define PORT 8080
+
+
 int main(int argc, const char *argv[]) {
-    // Create a socket
+	// Create a socket
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1) {
         perror("webserver (socket)");
         return 1;
     }
-	
     printf("socket created successfully\n");
+
+    // Create the address to bind the socket to
+    struct sockaddr_in host_addr;
+    int host_addrlen = sizeof(host_addr);
+
+    host_addr.sin_family = AF_INET;
+    host_addr.sin_port = htons(PORT);
+    host_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    // Bind the socket to the address
+    if (bind(sockfd, (struct sockaddr *)&host_addr, host_addrlen) != 0) {
+        perror("webserver (bind)");
+        return 1;
+    }
+    printf("socket successfully bound to address\n");
 
 	return 0;
 }
